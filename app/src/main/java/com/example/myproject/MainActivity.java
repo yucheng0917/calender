5f318d1bc4e6f7e64b10f6529c12163d9e63d662
jdjh7 <u10712027@ms.ttu.edu.tw>
2021-03-22 10:18:46 +0800

    my project

---
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+
+    public void registered(View view){
+
+        Intent intent=new Intent(MainActivity.this, login2.class);
+        startActivity(intent);
+
+    }
+
+}
---
+++ app/src/main/java/com/example/myproject/login2.java
@@ -0,0 +1,12 @@
+package com.example.myproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class login2 extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login2);
+    }
+}
